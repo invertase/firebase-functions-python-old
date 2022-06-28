00a3a70676e9c3fc47903de25ae91b7fece3b141
pr_Mais <mais.alheraki@gmail.com>
2022-06-28 15:34:57 +0300

    fix: add yaml to requirmenets

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-install_requires = ['flask>=2.1.2', 'functions-framework>=3.0.0']
+install_requires = ['flask>=2.1.2', 'functions-framework>=3.0.0', 'pyyaml>=5.1']
 
 setup(
     name='firebase_functions',
