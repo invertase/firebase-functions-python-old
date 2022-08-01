bc10c0eb38c468dcdba633d6df0eb95ad491e01d
Salakar <mike.diarmid@gmail.com>
2022-08-01 11:45:31 +0100

    -

--- e2e/python.test.js
+++ e2e/python.test.js
@@ -27,44 +27,44 @@ describe("firebase-functions-python", () => {
     unauthenticatedInstance = getFunctions(unauthenticatedApp);
   });
 
-  test("accepts a primitive arg of `undefined`", () => {
-    const callable = httpsCallable(instance, "todo");
-    const { data } = await callable();
-    expect(data).toBe('None');
-  });
+  // test("accepts a primitive arg of `undefined`", () => {
+  //   const callable = httpsCallable(instance, "testfunctiondefaultregion");
+  //   const { data } = await callable();
+  //   expect(data).toBe('None');
+  // });
 
-  test("accepts a primitive arg of `string`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts a primitive arg of `string`", async () => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable('foo');
     expect(data).toBe('string');
   });
 
-  test("accepts a primitive arg of `number`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts a primitive arg of `number`", async () => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable(123);
     expect(data).toBe('number');
   });
 
-  test("accepts a primitive arg of `boolean`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts a primitive arg of `boolean`", async() => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable(true);
     expect(data).toBe('boolean');
   });
 
-  test("accepts a primitive arg of `null`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts a primitive arg of `null`", async () => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable(null);
     expect(data).toBe('None');
   });
 
-  test("accepts an arg of `array`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts an arg of `array`", async () => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable([1, '2', true]);
     expect(data).toBe('array');
   });
 
-  test("accepts an arg of `object`", () => {
-    const callable = httpsCallable(instance, "todo");
+  test("accepts an arg of `object`", async () => {
+    const callable = httpsCallable(instance, "testfunctiondefaultregion");
     const { data } = await callable({ foo: 'bar' });
     expect(data).toBe('object');
   });
