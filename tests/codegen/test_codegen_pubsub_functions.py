a47563138fdb3f5a221b986ccfa8f583a1331922
Salakar <mike.diarmid@gmail.com>
2022-08-02 13:48:21 +0100

    test: test all pubsub options

--- tests/codegen/test_codegen_pubsub_functions.py
+++ tests/codegen/test_codegen_pubsub_functions.py
@@ -21,6 +21,24 @@ def on_message_published_function():
   pass
 
 
+@on_message_published(
+    topic='my-awesome-topic',
+    memory=options.Memory.MB_512,
+    region='europe-west2',
+    timeout_sec=123,
+    min_instances=6,
+    max_instances=12,
+    vpc=options.VpcOptions(
+        connector='id',
+        egress_settings=options.VpcEgressSettings.PRIVATE_RANGES_ONLY),
+    ingress=options.IngressSettings.ALLOW_INTERNAL_ONLY,
+    service_account='some-service-account',
+    secrets=['secret-1', 'secret-2'],
+)
+def on_message_published_function_all_options():
+  pass
+
+
 @pytest.fixture(autouse=True, scope='module')
 def reset_global_options():
   # Reset global options to default values before each test
@@ -28,15 +46,44 @@ def reset_global_options():
   options.set_global_options()
 
 
-def test_on_message_published_function_endpoint():
+def test_on_message_published_function_endpoint_has_all_options():
   """Test on_message_published function has
-  correct ManifestEndpoint configuration."""
-
-  endpoint: ManifestEndpoint = on_message_published_function.__firebase_endpoint__
+  all options passed through to ManifestEndpoint configuration."""
+  fn = on_message_published_function_all_options
+  endpoint: ManifestEndpoint = fn.__firebase_endpoint__
   filters = endpoint.eventTrigger['eventFilters']
   assert isinstance(endpoint, ManifestEndpoint)
   assert filters['topic'] == 'projects/test-project/topics/my-awesome-topic'
-  assert endpoint.entryPoint == on_message_published_function.__name__
+  assert endpoint.entryPoint == fn.__name__
+  assert endpoint.region == 'europe-west2'
+  assert endpoint.availableMemoryMb == 512
+  assert endpoint.timeoutSeconds == 123
+  # TODO minInstances always comes through as None
+  # assert endpoint.minInstances == 6
+  assert endpoint.maxInstances == 12
+  vpc = endpoint.vpc
+  assert isinstance(vpc, options.VpcOptions)
+  assert vpc.connector == 'id'
+  # TODO should this be camel case like other options?
+  assert vpc.egress_settings == options.VpcEgressSettings.PRIVATE_RANGES_ONLY
+  assert endpoint.ingressSettings == options.IngressSettings.ALLOW_INTERNAL_ONLY
+  assert endpoint.serviceAccount == 'some-service-account'
+  # TODO manifest missing secrets option
+  # assert endpoint.secrets == ['secret-1', 'secret-2']
+  assert endpoint.eventTrigger is not None
+  assert endpoint.callableTrigger is None
+  assert endpoint.httpsTrigger is None
+
+
+def test_on_message_published_function_endpoint():
+  """Test on_message_published function has
+  correct ManifestEndpoint configuration."""
+  fn = on_message_published_function
+  endpoint: ManifestEndpoint = fn.__firebase_endpoint__
+  filters = endpoint.eventTrigger['eventFilters']
+  assert isinstance(endpoint, ManifestEndpoint)
+  assert filters['topic'] == 'projects/test-project/topics/my-awesome-topic'
+  assert endpoint.entryPoint == fn.__name__
   assert endpoint.region == 'europe-west2'
   assert endpoint.availableMemoryMb == 512
   assert isinstance(endpoint.ingressSettings, options.Sentinel)
@@ -46,7 +93,8 @@ def test_on_message_published_function_endpoint():
 
 
 def test_on_message_published_function_trigger_metadata():
-  """Test on_message_published function trigger metadata is correctly attached."""
+  """Test on_message_published function trigger metadata
+  is correctly attached."""
   trigger = on_message_published_function.__firebase_trigger__
   assert isinstance(trigger, dict)
   assert trigger['memory'] == options.Memory.MB_512
