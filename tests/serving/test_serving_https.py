37180a7852be89c62d550b6d12b365ed65ec9ed1
pr_Mais <mais.alheraki@gmail.com>
2022-08-09 14:26:08 +0300

    fix

--- tests/serving/test_serving_https.py
+++ tests/serving/test_serving_https.py
@@ -30,23 +30,19 @@ os.environ["GCLOUD_PROJECT"] = "test-project"
     ),
 )
 def http_request_function(_, res: Response):
-  res.set_data('Hello World!')
+    """Gather HTTP request response"""
+    res.set_data("Hello World!")
 
 
 @on_call(memory=options.Memory.MB_256)
 def http_callable_function(_):
-  return 'Hello World, again!'
+    return "Hello World, again!"
 
-=======
-def http_request_function(req, res: Response):
-    """Gather HTTP request response"""
-    LOGGER.debug(req.data.decode("utf-8"))
-    res.set_data("Hello World!")
 
 triggers = {}
 
-triggers['http_request_function'] = http_request_function
-triggers['http_callable_function'] = http_callable_function
+triggers["http_request_function"] = http_request_function
+triggers["http_callable_function"] = http_callable_function
 
 
 def test_admin_view_func():
@@ -54,18 +50,12 @@ def test_admin_view_func():
     with serve_admin(triggers=triggers).test_client() as client:
         res = client.get("/__/functions.yaml")
         assert res.status_code == 200, "response failure, status_code != 200 "
-        assert (
-            yaml.safe_load(res.get_data())["endpoints"]["httprequestfunction"][
-                "httpsTrigger"
-            ]
-            is not None
-        ), "Failure, httpsTrigger is none"
-        assert (
-            yaml.safe_load(res.get_data())["endpoints"]["httpcallablefunction"][
-                "callableTrigger"
-            ]
-            is not None
-        ), "Failure, callableTrigger is none"
+        assert (yaml.safe_load(
+            res.get_data())["endpoints"]["httprequestfunction"]["httpsTrigger"]
+                is not None), "Failure, httpsTrigger is none"
+        assert (yaml.safe_load(res.get_data())["endpoints"]
+                ["httpcallablefunction"]["callableTrigger"]
+                is not None), "Failure, callableTrigger is none"
 
 
 def test_trigger_view_func():
@@ -77,9 +67,8 @@ def test_trigger_view_func():
             content_type="application/json",
         )
 
-        assert (
-            res_request.data.decode("utf-8") == "Hello World!"
-        ), 'Discrepancy found, response data != "Hello World!"'
+        assert (res_request.data.decode("utf-8") == "Hello World!"
+               ), "Discrepancy found, response data != 'Hello World!'"
 
         res_call = client.post(
             "/http_callable_function",
@@ -89,9 +78,9 @@ def test_trigger_view_func():
 
         # Authenticated missing request
         assert (
-            json.loads(res_call.data.decode("utf-8")).get("data")
-            == "Hello World, again!"
-        ), 'Unauthenticated response or found request, response data != "Hello World!, again!"'
+            json.loads(res_call.data.decode("utf-8")).get(
+                "data") == "Hello World, again!"
+        ), "Unauthenticated response or found request, response data != 'Hello World!, again!'"
 
         res_call = client.post(
             "/http_callable_function",
@@ -100,18 +89,16 @@ def test_trigger_view_func():
             content_type="application/json",
         )
         # Unauthenticated request
-        assert (
-            json.loads(res_call.data.decode("utf-8")).get("error")["message"]
-            == "Unauthenticated"
-        ), 'Authenticated response, error message != "Unauthenticated"'
+        assert (json.loads(res_call.data.decode("utf-8")).get(
+            "error")["message"] == "Unauthenticated"
+               ), "Authenticated response, error message != 'Unauthenticated'"
 
 
 def test_quit_view_func():
     """Tests for quit view function response"""
     with serve_admin(triggers=triggers).test_client() as client:
-        assert (
-            client.get("/__/quitquitquit").status_code == 200
-        ), "response failure, status_code != 200"
+        assert (client.get("/__/quitquitquit").status_code == 200
+               ), "response failure, status_code != 200"
 
 
 def test_asdict_factory_cleanup():
@@ -126,10 +113,8 @@ def test_asdict_factory_cleanup():
         dict_factory=clean_nones_and_set_defult,
     )
 
-    assert (
-        manifest["endpoints"]["httprequestfunction"]["maxInstances"] is None
-    ), "Failure, maxInstances is not none"
+    assert (manifest["endpoints"]["httprequestfunction"]["maxInstances"] is
+            None), "Failure, maxInstances is not none"
     with pytest.raises(KeyError):
-        assert (
-            manifest["endpoints"]["httpcallablefunction"]["vpc"] is None
-        ), "Failure, vpc is not none"
+        assert (manifest["endpoints"]["httpcallablefunction"]["vpc"] is
+                None), "Failure, vpc is not none"
