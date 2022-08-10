1e624dc551921c0c629c25ae6ea25b6b7034ea1e
gustavo-varollo <gustavo_varollo@hotmail.com>
2022-08-10 16:18:40 +0100

    Syntax review test params and add error messages

--- tests/params/test_int_param.py
+++ tests/params/test_int_param.py
@@ -6,15 +6,16 @@ from firebase_functions import params
 def test_int_param_value():
     """Testing if int param correctly returns a value."""
     os.environ["int_value_test"] = "123"
-    assert params.IntParam("int_value_test").value() == 123
+    assert params.IntParam("int_value_test").value() == 123, "Failure, prams value != 123"
 
 
 def test_int_param_empty_default():
     """Testing if int param defaults to empty int if no value and no default."""
-    assert params.IntParam("int_default_test").value() == int()
+    assert params.IntParam("int_default_test").value() == int(), "Failure, prams value is not int"
 
 
 def test_int_param_default():
     """Testing if int param defaults to provided default value."""
     # TODO this isn't showing type issues when not an int
-    assert params.IntParam("int_default_test", default=456).value() == 456
+    assert params.IntParam("int_default_test", default=456).value() == 456,\
+        "Failure, prams defult value != 456"
--- tests/params/test_string_param.py
+++ tests/params/test_string_param.py
@@ -1,32 +1,23 @@
-"""
-StringParam unit tests.
-"""
+"""StringParam unit tests."""
 import os
-import firebase_functions.params as params
+from firebase_functions import params
 
 
 def test_string_param_value():
-    """
-    Testing if string param correctly returns a value.
-    """
+    """Testing if string param correctly returns a value."""
     os.environ["string_value_test"] = "string_test"
-    param = params.StringParam("string_value_test")
-    assert param.value() == "string_test"
+    assert params.StringParam("string_value_test").value() == "string_test",\
+        'Failure, prams value != "string_test"'
 
 
 def test_string_param_empty_default():
-    """
-    Testing if string param defaults to empty string if no value and no default.
-    """
-    param = params.StringParam("string_default_test")
-    assert param.value() == ""
+    """Testing if string param defaults to empty string if no value and no default."""
+    assert params.StringParam("string_default_test").value() == str(),\
+        "Failure, prams value is not a string"
 
 
 def test_string_param_default():
-    """
-    Testing if string param defaults to provided default value.
-    """
-    param = params.StringParam(
-        "string_default_test", default="string_overriden_default"
-    )
-    assert param.value() == "string_overriden_default"
+    """Testing if string param defaults to provided default value."""
+    assert (params.StringParam("string_default_test", default="string_override_default").value()
+            == "string_override_default"), \
+        'Failure, prams default value != "string_override_default"'
